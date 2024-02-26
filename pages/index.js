5c11ad19c822c2c7d8387b2f30b61ced437da16d
evan <malcomchege0582@gmail.com>
2024-02-26 14:20:25 +0300

    fetch tours

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 import Loading from "@/components/Loading";
 import Tour from "@/components/Tour";
+import Tours from "@/components/Tours";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 const url = 'https://www.course-api.com/react-tours-project'
 
 export default function Home() {
   const [loading,setLoading] = useState(true)
   const [tours,setTours] = useState([])
+  const fetchTours = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false)
+      setTours(tours)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+   
+  }
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  
   if(loading){
     return (
       <main>
@@ -16,7 +34,7 @@ export default function Home() {
   }
   return (
     <main>
-      <h1>tours</h1>
+     <Tours/>
       
       
     </main>
